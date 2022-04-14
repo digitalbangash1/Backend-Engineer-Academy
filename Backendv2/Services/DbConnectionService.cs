0fb7061962d28fb387d59bc0ea27ed198d5d8f99
salim963 <salimdk97@gmail.com>
2022-04-14 23:29:21 +0200

    ..

--- Backendv2/Services/DbConnectionService.cs
+++ Backendv2/Services/DbConnectionService.cs
@@ -6,7 +6,7 @@ namespace Backendv2.Services
     public class DbConnectionService : IDbConnectionService
     {
         //TODO this should not be in code but in appsettings.json
-        private static string connectionString = "server=localhost;port=3306;database=backend;user=root;password=12345";
+        private static string connectionString = "server=localhost;port=3306;database=backend;user=root;password=salim123";
         //
         //"server=130.225.170.79;uid=bangash;pwd=123;database=backend";
         //"server=130.225.170.79;database=backend;user=bangash;password=123";
